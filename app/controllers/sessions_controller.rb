cbfff1acefc594bce05588c2307f5bc4082d1511
Alexander Borovykh <immaculate.pine@gmail.com>
2014-03-12 20:55:17 +0400

    Use permanent cookies

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ActionController::Base
   # POST /sessions
   def create
     login_info = Uas::User.login(params[:phone], params[:password])
-    cookies[:auth_token] = { value: login_info.token, domain: Rails.configuration.auth_domain }
+    cookies.permanent[:auth_token] = { value: login_info.token, domain: Rails.configuration.auth_domain }
     render json: { redirect_to: topics_url }
   rescue Uas::Error
     head :unprocessable_entity
