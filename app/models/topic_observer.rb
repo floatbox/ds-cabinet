ca6473aed92f5bb0abc185bda2fee97593fd2ec1
Alexander Borovykh <immaculate.pine@gmail.com>
2014-03-31 12:51:34 +0400

    CCDEV-35 Do not send notifications about topics created by concierge

--- app/models/topic_observer.rb
+++ app/models/topic_observer.rb
@@ -1,6 +1,7 @@
 class TopicObserver < ActiveRecord::Observer
   def after_create(record)
-    if record.user.concierge
+    # Send notification to the user's concierge (unless concierge is the author of the topic)
+    if record.user.concierge && record.user.concierge_id != record.author_id
       Notification.create(
         user: record.user.concierge,
         object: record,
