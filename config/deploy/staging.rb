64fd99bc34a866f4b8752a7f97188711bb5083fb
Alexander Borovykh <immaculate.pine@gmail.com>
2014-04-14 13:30:36 +0400

    Updated deploy configs for new server

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -4,9 +4,9 @@
 # server in each group is considered to be the first
 # unless any hosts have the primary property set.
 # Don't declare `role :all`, it's a meta role
-role :app, %w{w3dev-ds-cabinet@ono.rrv.ru}
-role :web, %w{w3dev-ds-cabinet@ono.rrv.ru}
-role :db,  %w{w3dev-ds-cabinet@ono.rrv.ru}
+role :app, %w{w3dev-ds-cabinet@ono.rrv.ru:2223}
+role :web, %w{w3dev-ds-cabinet@ono.rrv.ru:2223}
+role :db,  %w{w3dev-ds-cabinet@ono.rrv.ru:2223}
 
 set :deploy_to, '/www/dev-ds-cabinet.onomnenado.ru'
 set :branch, 'feature/chat'
