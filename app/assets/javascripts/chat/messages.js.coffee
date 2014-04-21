52f29c09730210bdc50fde989f18ad8359293071
Alexander Borovykh <immaculate.pine@gmail.com>
2014-04-21 17:27:38 +0400

    CCDEV-52 Attachment file size validation added

--- app/assets/javascripts/chat/messages.js.coffee
+++ app/assets/javascripts/chat/messages.js.coffee
@@ -4,5 +4,8 @@ $ ->
       dataType: 'script'
       dropZone: $(this).find('.dropfile')
       add: (e, data) ->
-        $(this).addClass('uploading');
+        $(this).addClass('uploading')
+        $(this).fadeTo('fast', 0.5)
         data.submit()
+      done: (e, data) ->
+        $(this).fadeTo('fast', 1.0)
--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -1,4 +1,18 @@
+require 'file_size_validator'
+
 class Attachment < ActiveRecord::Base
   has_and_belongs_to_many :messages
   mount_uploader :attachment, AttachmentUploader
+  validates :attachment,
+    presence: true,
+    file_size: { maximum: 10.megabytes.to_i }
+
+  # return [String] error messages that could be displayed in alert box
+  def errors_as_text
+    errors.messages.map do |attribute, messages|
+      attribute_name = I18n.t("activerecord.attributes.#{self.class.name.to_s.singularize.underscore}.#{attribute}")
+      "#{attribute_name} #{messages.join(', ')}."
+    end.join(' ')
+  end
+
 end
