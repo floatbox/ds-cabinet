7b879214b53854e3f6c49d2501eeeeb67e10eebb
Alexander Borovykh <immaculate.pine@gmail.com>
2014-04-22 15:15:51 +0400

    CCDEV-32 Split texts by paragraphs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  # Adds HTML markup to the plane text with paragraphs separated by new lines.
+  # @param text [String] original text
+  # @return [ActiveSupport::SafeBuffer] HTML safe text splitted by <p> tags
+  def paragraphs(text)
+    paragraphs = text.split("\n").map(&:strip).select(&:present?)
+    paragraphs.map{|p| "<p>#{p}</p>"}.join("\r\n").html_safe
+  end
+
 end
