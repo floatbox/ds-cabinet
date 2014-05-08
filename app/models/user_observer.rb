5c6c3bc4344130c970fe35165fcef5cb2beadc1c
Alexander Borovykh <immaculate.pine@gmail.com>
2014-05-08 12:01:46 +0400

    CCDEV-114 Create welcome topic for new users

--- app/models/user_observer.rb
+++ app/models/user_observer.rb
@@ -1,13 +1,8 @@
 class UserObserver < ActiveRecord::Observer
   
   def after_create(user)
-    User.concierges.each do |concierge|
-      Notification.create(
-        user: concierge,
-        object: user,
-        name: 'user_created'
-      )
-    end
+    notify_conierges_about_new_user(user)
+    create_welcome_topic(user)
   end
 
   def after_update(user)
@@ -16,13 +11,31 @@ class UserObserver < ActiveRecord::Observer
 
   private
 
-    # Send notification to the concierge that user was attached to
+    # Sends notification to all concierges about new user
+    def notify_conierges_about_new_user(user)
+      User.concierges.each do |concierge|
+        Notification.create(
+          user: concierge,
+          object: user,
+          name: 'user_created'
+        )
+      end
+    end
+
+    # Sends notification to the concierge that user was attached to
     def user_attached(user)
       Notification.create(
         user: user.concierge,
         object: user,
         name: 'user_attached'
       )
+    end
+
+    # Creates welcome topic from random concierge to new user
+    def create_welcome_topic(user)
+      concierge = User.concierges.order('RANDOM()').limit(1).first
+      text = I18n.t('system_messages.welcome_topic')
+      user.topics.create(text: text, author_id: concierge.id)
     end
 
 end
