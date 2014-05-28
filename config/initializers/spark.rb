23946e225a7b7a628662cd818536752484ebc232
Alexander Borovykh <immaculate.pine@gmail.com>
2014-05-28 13:09:50 +0400

    Production Spark

--- config/initializers/spark.rb
+++ config/initializers/spark.rb
@@ -12,7 +12,7 @@ end
 
 # TODO: Change to production credentials
 Ds::Spark.configure :production do
-  wsdl 'http://sparkgatetest.interfax.ru/IfaxWebService/ifaxwebservice.asmx?WSDL'
-  login 'ds_gate'
-  password 'Vz3JW9h'
+  wsdl 'http://webservicefarm.interfax.ru/iFaxWebService/ifaxwebservice.asmx?WSDL'
+  login 'DasredaGate'
+  password 'OBCzyty'
 end
