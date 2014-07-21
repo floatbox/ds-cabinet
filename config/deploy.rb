d8148080a2075d889445deba4ea190db986a2938
Ruslan Mamaev <dietek@yandex.ru>
2014-07-21 17:55:07 +0400

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.1.0'
 
 set :application, 'ds-cabinet'
-set :repo_url, 'git@github.com:SuperbCoders/ds-cabinet.git'
+set :repo_url, 'git@github.com:BusinessEnvironment/ds-cabinet.git'
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
