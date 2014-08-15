3ce5df88649378dba7f4cb4c924387a502dd4b30
Zhan Tuaev <zmtuaev@dasreda.ru>
2014-08-15 14:14:45 +0400

    Implement SimpleSerializer, extend Uas::User with it

--- lib/simple_serializer.rb
+++ lib/simple_serializer.rb
@@ -4,6 +4,8 @@ module SimpleSerializer
   # Serialization to store object in Registration#uas_user attribute
 
   def dump obj
+    return nil.to_yaml if obj.nil?
+
     unless obj.is_a?(self)
       raise ::ActiveRecord::SerializationTypeMismatch,
         "Attribute was supposed to be a #{self}, but was a #{obj.class}. -- #{obj.inspect}"
@@ -18,7 +20,7 @@ module SimpleSerializer
 
   def load string
     string.nil? ? nil : self.new.tap do |user|
-      YAML.load(string).each do |key, value|
+      (YAML.load(string) || {}).each do |key, value|
         user.instance_variable_set(key, value)
       end
     end
