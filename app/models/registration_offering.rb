d196db844204bafec9be98f38096e2811c66b77f
Zhan Tuaev <zmtuaev@dasreda.ru>
2014-08-19 15:48:45 +0400

    Add :code attribute to PimOffering

--- app/models/registration_offering.rb
+++ app/models/registration_offering.rb
@@ -1,6 +1,7 @@
-require 'ds/pim/registration_prices'
+require 'ds/pim'
 
 class RegistrationOffering< PimOffering
+  after_initialize :init
 
   # Возвращает дату истечения продукта относительно даты from_date.
   #   Рассчитывается из даты from_date, периода и кол-ва периодов
@@ -20,10 +21,31 @@ class RegistrationOffering< PimOffering
   def self.reload_from_pim offering_id
     self.destroy_all
     rp = Ds::Pim::RegistrationPrices.new offering_id
-    rp.prices.each do |price|
-      attributes = price.to_h
-      #{:offering_id=>"3000319", :price_id=>"3000329", :price_text=>"3 месяца", :price_unit=>"Month", :price_unit_qty=>3, :price_amount=>2400.0} 
-      self.create(attributes)
+    self.transaction do
+      rp.prices.each do |price|
+        attributes = price.to_h
+        #{
+        #  :offering_id=>"3000319", 
+        #  :price_id=>"3000329", 
+        #  :price_text=>"3 месяца", 
+        #  :price_unit=>"Month", 
+        #  :price_unit_qty=>3, 
+        #  :price_amount=>2400.0
+        #}
+        
+        self.create(attributes)
+      end
     end
+  end
+
+  def self.as_hash # to use in controllers, views and partials
+    self.order('price_amount ASC').all.
+      map{ |e| { 
+                 price_id:     e.price_id, 
+                 price_text:   e.price_text, 
+                 price_amount: e.price_amount,
+                 code:         e.code
+               }
+         }
   end
 end
