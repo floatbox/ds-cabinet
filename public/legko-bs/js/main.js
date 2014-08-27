a0237b90a338b514e1a4fc8f155bc492ab189e13
Zhan Tuaev <zmtuaev@dasreda.ru>
2014-08-27 15:06:06 +0400

    Replace applcation template with new one, Add public/legko-bs

---
+++ public/legko-bs/js/main.js
@@ -0,0 +1,44 @@
+$(function(){
+	var body = $("html, body");
+	if($('.js__switcher').length){
+
+		$('.js__switcher').switcher();
+		
+		var a='switch_status',
+			b='switch_field',
+			s1='Я являюсь ИП',
+			s2='Я не являюсь ИП';
+		
+		$('#switch_btn1').click(function(){
+			$('#'+a).html(s1);
+			$('#'+b).show();
+			});
+			
+		$('#switch_btn2').click(function(){
+			$('#'+a).html(s2);
+			$('#'+b).hide();
+			});
+	};
+	
+	$('.case-intro .ttl').click(function(){
+		var class_fl = $(this).attr('data-fl');
+		$('.case-intro .ttl').removeClass('active');
+		$(this).addClass('active');
+		$('#sw_fields > tbody > tr').hide();
+		$('.'+class_fl).show();
+	});
+	
+	$('.case-regin .reg').click(function(){
+		$('.ttl[data-fl="regi_fl"]').trigger('click');
+		body.animate({scrollTop:0}, 500, 'swing', function() { 
+		});
+	});
+	
+	$('.case-regin .enter').click(function(){
+		$('.ttl[data-fl="enter_fl"]').trigger('click');
+		body.animate({scrollTop:0}, 500, 'swing', function() { 
+		});
+	});
+});
+
+
