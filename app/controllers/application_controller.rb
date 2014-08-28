3f1ad6b5373ff044c3ed38766fca2bdb1d0be000
Zhan Tuaev <zmtuaev@dasreda.ru>
2014-08-28 17:41:38 +0400

    Cut down application layout to partials

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  attr_accessor :article_css_class
+  helper_method :article_css_class
 
   after_action :update_last_activity
 
