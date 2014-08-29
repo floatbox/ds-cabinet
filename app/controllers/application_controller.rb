845250e43f67cdb8e4680690892a2b89febb37cc
Zhan Tuaev <zmtuaev@dasreda.ru>
2014-08-29 17:40:13 +0400

    Fix article_css_class issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   
-  attr_accessor :article_css_class
-  helper_method :article_css_class
-
   after_action :update_last_activity
 
   rescue_from CanCan::AccessDenied do |exception|
