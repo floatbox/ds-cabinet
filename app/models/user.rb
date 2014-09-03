258ddb4967338a7430b9a6debe0d44c7a132c858
Andrey Arefiev <aaarefiev@dasreda.ru>
2014-09-03 15:07:45 +0400

    выключил Siebel в dev режиме

--- app/models/user.rb
+++ app/models/user.rb
@@ -46,7 +46,7 @@ class User < ActiveRecord::Base
 
   # @return [Contact] Siebel representation of user
   def siebel
-    # return Contact.new
+    return Contact.new if Rails.env == 'development'
     @siebel ||= if siebel_id
       Contact.find(siebel_id)
     else
