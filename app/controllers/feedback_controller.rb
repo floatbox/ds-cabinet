f479ebc495323d3bd9a97cad7392ac3d80aa1f7b
Zhan Tuaev <zmtuaev@dasreda.ru>
2014-09-10 17:31:07 +0400

    Implement sending email in feedback controller

--- app/controllers/feedback_controller.rb
+++ app/controllers/feedback_controller.rb
@@ -2,7 +2,13 @@ class FeedbackController < ApplicationController
   respond_to :json
 
   def create
-    #render json: { message: t('.success') }
-    render json: { base: [t('.fail')] }, status: :unprocessable_entity
+    begin
+      RegistrationMailer.feedback_email(params).deliver
+
+      render json: { message: t('.success') }
+    rescue => exception
+      logger.error "Error occured. #{exception.message}"
+      render json: { base: [t('.fail')] }, status: :unprocessable_entity
+    end
   end
 end
