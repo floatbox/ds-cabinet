f64b4fdc551fdc867d47c891b304ae2e6d155009
Zhan Tuaev <zmtuaev@dasreda.ru>
2014-09-24 19:46:13 +0400

    Implement VCR custom request matcher for Uas and Spark

--- features/support/vcr_helper.rb
+++ features/support/vcr_helper.rb
@@ -3,6 +3,48 @@ require 'savon'
 require 'vcr'
 
 VCR.configure do |c|
+  c.register_request_matcher :external_api_matcher do |req1, req2|
+    #JSON.parse(URI(req1.uri).to_json)
+    #{
+    #      "scheme" => "http",
+    #        "user" => nil,
+    #    "password" => nil,
+    #        "host" => "sparkgatetest.interfax.ru",
+    #        "port" => 80,
+    #        "path" => "/IfaxWebService/ifaxwebservice.asmx",
+    #       "query" => "WSDL",
+    #      "opaque" => nil,
+    #    "registry" => nil,
+    #    "fragment" => nil,
+    #      "parser" => nil
+    #}
+    if req1.uri     == req2.uri &&
+       req1.method  == req2.method
+
+      case URI(req2.uri).path
+        when %r(/SMSGateway/sms)
+          # all requests for sms gateway are identical for testing purpose
+          true
+
+        when %r(/IfaxWebService/ifaxwebservice.asmx) # Spark
+          req1.body  == req2.body &&
+          req1.headers["Soapaction"]   == req2.headers["Soapaction"]   &&
+          req1.headers["Content-Type"] == req2.headers["Content-Type"]
+        
+        when %r(/authentication/user/login/) # Uas
+          req1.body == req2.body
+
+        else 
+          req1.headers == req2.headers &&
+          req1.body    == req2.body 
+      end
+    else
+      false
+    end
+  end
+end
+
+VCR.configure do |c|
   c.cassette_library_dir = 'features/vcr_cassettes'
   c.hook_into :webmock
   #http://stackoverflow.com/questions/20667642/rspec-savon-vcr-not-recording
@@ -12,6 +54,7 @@ VCR.configure do |c|
   c.default_cassette_options                = { 
     record: :new_episodes, 
     allow_playback_repeats: true, 
-    match_requests_on: [:method, :uri, :headers, :body] 
+    match_requests_on: [:external_api_matcher]
+    #match_requests_on: [:method, :uri, :headers, :body] 
   }
 end
