5a20f51daf797e1f92de467f3a41b51ba1854de3
Zhan Tuaev <zmtuaev@dasreda.ru>
2014-09-24 16:55:32 +0400

    Add new step for waiting for Ajax to complete

--- features/step_definitions/basic_steps.rb
+++ features/step_definitions/basic_steps.rb
@@ -91,6 +91,12 @@ end
   page.evaluate_script('jQuery.active == 0').should == (negation == 'не ' ? true : false)
 end
 
+То(/^ждать завершения всех Ajax запросов$/) do
+  while page.evaluate_script('jQuery.active != 0')
+    sleep 0.1
+  end
+end
+
 То(/^появляется сообщение об ошибке "(.*?)"$/) do |message|
   should have_selector('#myModal', text:message)
 end
