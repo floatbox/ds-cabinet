752f784656c4a86052c84235908f42076e232d9a
Zhan Tuaev <zmtuaev@dasreda.ru>
2014-09-25 16:23:58 +0400

    Add selectors for Dialog and registration confirmation form

--- features/support/selectors_helpers.rb
+++ features/support/selectors_helpers.rb
@@ -4,8 +4,10 @@ module SelectorsHelpers
     when /шапк(?:а|е)/                     then 'header.page-header'
     when /контент(?:е|)/                    then 'section.page-body'
     when /подвал(?:е)/                     then 'footer.page-footer'
+    when /сообщени(?:е|и|я)/               then '#myModal'
     when /форм(?:а|е) регистрации и входа/ then '.js-forms_container'
     when /форм(?:а|е) обратной связи/      then '.js-feedback_form'
+    when /форм(?:а|е) подтверждения регистрации/ then '.js-confirmation_form'
     else
       raise "Unknown area: #{name}, should be either шапк(а|е), контент(е), подвал(е) или форм(а|е) регистрации и входа или форм(а|е) обратной связи"
     end
