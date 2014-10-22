94716149cf9795cbef0f7c95345ee1bfd3aebb71
Zhan Tuaev <zmtuaev@dasreda.ru>
2014-10-22 14:00:10 +0400

    Fix test breaking merge 5b86665f

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -30,6 +30,7 @@ DsCabinet::Application.configure do
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
+  config.registration_mail_to = 'Legko_support@dasreda.ru'
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
